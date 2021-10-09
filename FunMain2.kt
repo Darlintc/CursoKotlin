f5bf942cc953dc9148be7ac37bd45d973ae5642d
darlintc <darlintapia09@hotmail.com>
2021-10-08 22:37:43 -0600

    agregamos comentios y salto de lineas

---
+++ FunMain2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    println("Happy Birthday, Ailan!")
+    // Esto es una line del cumple
+    println("You are already 5!")
+    println("5 is the very best age to celebrate")
+    // Let 's print cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+   // This prints an empty line.
+    println("")
+ 
+}
